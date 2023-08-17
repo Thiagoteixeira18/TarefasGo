c94431f2d181631d7bc5dac63f0a542bfdadaa8e
Thiagoteixeira18 <thiagoteixeira.epo@gmail.com>
2023-08-16 21:44:39 -0300

    adicionado controllers login

--- WebApp/src/router/rotas/login.go
+++ WebApp/src/router/rotas/login.go
@@ -18,4 +18,10 @@ var RotasLogin = []Rota{
 		Funcao:             controllers.CarregarTelaDeLogin,
 		RequerAutenticacao: false,
 	},
+	{
+		Uri:                "/login",
+		Metodo:             http.MethodPost,
+		Funcao:             controllers.FazerLogin,
+		RequerAutenticacao: false,
+	},
 }
